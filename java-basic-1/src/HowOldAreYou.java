a306afbc1f5ce5d5f7d13769604d8e01b3293019
uhosamurai <kirito3190@icloud.com>
2024-04-19 13:04:48 +0900

    a

---
+++ java-basic-1/src/HowOldAreYou.java
@@ -0,0 +1,41 @@
+// C言語では、#include に相当する
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HowOldAreYou {
+
+	public static void main(String[] args) { 
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			// BufferedReader というのは、データ読み込みのクラス(型)
+			// クラスの変数を作るには、new を使う。
+			// readLine() は、入出力エラーの可能性がある。エラー処理がないとコンパイルできない。
+			//  Java では、 try{ XXXXXXXX }  catch(エラーの型 変数) { XXXXXXXXXXXXXXXXXX} と書く
+		try {
+			System.out.println("何歳ですか?");
+			String line = reader.readLine();
+			int age = Integer.parseInt(line);
+            while(true){
+			System.out.println("2030年のあなたは" + (age+6) + "歳ですね。");
+            if(age<=34){
+             System.out.println("2030年のあなたは" + (age+6) + "歳ですね。");
+             System.out.println("あなたが誕生した年は+(age)+はですね。");
+
+            }
+			System.out.println("あなたが誕生した年ははですね。");
+            }
+        }
+		catch(IOException e) {
+			System.out.println(e);
+		}
+
+
+	}
+}
+
+//  課題    キーボードから数字を打ち込む
+//  その結果、 あなたは、???歳ですね、と画面に表示させる。
+//  その後、あなたは10年後、????歳ですね、と画面に表示させる。
+
+
