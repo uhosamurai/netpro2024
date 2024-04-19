4721a8c574ded724eabc60558ae2f95eecd7e057
uhosamurai <kirito3190@icloud.com>
2024-04-20 00:24:37 +0900

    修整版

--- java-basic-1/src/HowOldAreYou.java
+++ java-basic-1/src/HowOldAreYou.java
@@ -1,3 +1,4 @@
+
 // C言語では、#include に相当する
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -5,37 +6,52 @@ import java.io.InputStreamReader;
 
 public class HowOldAreYou {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			// BufferedReader というのは、データ読み込みのクラス(型)
-			// クラスの変数を作るには、new を使う。
-			// readLine() は、入出力エラーの可能性がある。エラー処理がないとコンパイルできない。
-			//  Java では、 try{ XXXXXXXX }  catch(エラーの型 変数) { XXXXXXXXXXXXXXXXXX} と書く
 		try {
+			while (true) {
 			System.out.println("何歳ですか?");
 			String line = reader.readLine();
-			int age = Integer.parseInt(line);
-            while(true){
-			System.out.println("2030年のあなたは" + (age+6) + "歳ですね。");
-            if(age<=34){
-             System.out.println("2030年のあなたは" + (age+6) + "歳ですね。");
-             System.out.println("あなたが誕生した年は+(age)+はですね。");
-
-            }
-			System.out.println("あなたが誕生した年ははですね。");
-            }
-        }
-		catch(IOException e) {
+			
+				if (line.equals("q") || line.equals("e")) {
+					break;
+				}
+				int age = 0;
+				try {
+					age = Integer.parseInt(line);
+				} catch (NumberFormatException e) {
+					System.out.println("数字を入力してください。");
+					continue;
+				}
+				if (age >= 120 || age <= 0) {
+					System.out.println("再入力してください");
+					continue;
+				}
+				System.out.println("2030年のあなたの年齢は" + (age + 7) + "です。");
+				System.out.println("あなたが誕生した年の元号は" + years(2023-age));
+			}
+		} catch (IOException e) {
 			System.out.println(e);
 		}
 
+	}
 
+	public static String years(int year) {
+		if (year >= 2019) {
+			return "令和" + (year - 2018) + "年です";
+		} else if (year >= 1989) {
+			return "平成" + (year - 1988) + "年です";
+		} else if (year >= 1926) {
+			return "昭和" + (year - 1925) + "年です。";
+		} else if (year >= 1912) {
+			return "大正" + (year - 1911) + "年です";
+		} else {
+			return "明治" + (year - 1866) + "年です";
+		}
 	}
 }
 
-//  課題    キーボードから数字を打ち込む
-//  その結果、 あなたは、???歳ですね、と画面に表示させる。
-//  その後、あなたは10年後、????歳ですね、と画面に表示させる。
-
-
+// 課題 キーボードから数字を打ち込む
+// その結果、 あなたは、???歳ですね、と画面に表示させる。
+// その後、あなたは10年後、????歳ですね、と画面に表示させる。
